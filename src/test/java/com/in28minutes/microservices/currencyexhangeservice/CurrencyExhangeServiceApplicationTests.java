461d60865805d6161fce8cb1bafb1dc45423d721
Nazli Ozcelik <nazli.ozcelik@mera.com>
2022-06-07 10:17:14 +0300

    Initial commit

---
+++ src/test/java/com/in28minutes/microservices/currencyexhangeservice/CurrencyExhangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currencyexhangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExhangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
